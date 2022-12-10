b5bc8ab096de8024657b64e29333a03f7bee120a
Corey Prophitt <git@prophitt.me>
2022-12-10 10:06:23 -0800

    Corrected the data structures and Train func

--- sexmachine.go
+++ sexmachine.go
@@ -9,35 +9,52 @@ const (
 	Female
 )
 
+type sex = int
+
+type names struct {
+	freqs map[string]float64
+	total int
+}
+
 // Classifier is used to store labeled data for classification.
 type Classifier struct {
-	Freqs map[int]map[string]float64 /* (Fe)Male -> { bob->2.0, ... } */
-	Total int                        /* size of entire population/observations */
+	data map[sex]names
 }
 
 // Train labels data and adds it to the classifier.
-func (c *Classifier) Train(label int, names ...string) {
+func (c *Classifier) Train(label sex, names ...string) {
 	for _, name := range names {
-		c.Freqs[label][normalize(name)]++
-		c.Total++
+		if d, ok := c.data[label]; ok {
+			d.freqs[normalize(name)]++
+			d.total++
+		}
 	}
 }
 
 // Predict takes a name and attempts to predict the person's sex using the classifier.
-func (c *Classifier) Predict(name string) (int, float64) {
+func (c *Classifier) Predict(name string) (sex, float64) {
+	name = parsename(name)
+
 	return 0, 0.0
 }
 
 // New creates a new classifier.
 func New() *Classifier {
 	return &Classifier{
-		Freqs: map[int]map[string]float64{
-			Male:   {},
-			Female: {},
+		data: map[sex]names{
+			Male: names{
+				freqs: map[string]float64{},
+			},
+			Female: names{
+				freqs: map[string]float64{},
+			},
 		},
 	}
 }
 
+// A helper function to naively parse a person's name. Assumes if there's more
+// than one name given, the first whole word is the person's given name. The
+// name is also normalized before being returned.
 func parsename(name string) string {
 	if fields := strings.Fields(name); len(fields) > 0 {
 		return normalize(fields[0])
@@ -46,6 +63,8 @@ func parsename(name string) string {
 	return ""
 }
 
+// A helper function to normalize a name (removed leading and trailing white
+// space and downcases the name).
 func normalize(s string) string {
 	return strings.TrimSpace(strings.ToLower(s))
 }
