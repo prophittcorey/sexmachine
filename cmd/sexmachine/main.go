828957b0ad33c7475e757ce0071094cd203b3d5d
Corey Prophitt <git@prophitt.me>
2022-12-14 06:47:04 -0800

    Implemented basic training

--- cmd/sexmachine/main.go
+++ cmd/sexmachine/main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
+	"fmt"
 	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"github.com/prophittcorey/sexmachine"
+)
+
+var (
+	errNoFiles = fmt.Errorf("error: one or more files are required for training")
 )
 
 func main() {
-	var data string
 	var classifier string
+	var data string
 
-	flag.StringVar(&data, "data", "", "a data file to train a classifier with")
 	flag.StringVar(&classifier, "classifier", "", "a classifier to create (or update if it already exists)")
+	flag.StringVar(&data, "data", "", "one or more files to train a classifier with (csv: name,sex,frequency")
 
 	flag.Parse()
 
@@ -25,6 +37,65 @@ func main() {
 	flag.Usage()
 }
 
-func train(classifier, data string) error {
+func train(classifierpath, glob string) error {
+	files, err := filepath.Glob(glob)
+
+	if err != nil {
+		return err
+	}
+
+	if len(files) == 0 {
+		return errNoFiles
+	}
+
+	classifier := sexmachine.New()
+
+	// TODO: load an existing classifier?
+
+	for _, f := range files {
+		(func() {
+			f, err := os.Open(f)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			defer f.Close()
+
+			reader := csv.NewReader(f)
+
+			data, err := reader.ReadAll()
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			sexes := map[byte]int{
+				'f': sexmachine.Female,
+				'm': sexmachine.Male,
+			}
+
+			for _, row := range data {
+				if len(row) < 3 {
+					continue
+				}
+
+				name, sex, frequency := row[0], strings.ToLower(row[1]), row[2]
+
+				if len(sex) == 0 || (sex[0] != 'f' && sex[0] != 'm') {
+					continue
+				}
+
+				if i, err := strconv.ParseInt(frequency, 10, 32); err == nil {
+					classifier.Observe(sexes[sex[0]], name, int(i))
+				}
+			}
+		})()
+	}
+
+	if err = classifier.SaveFile(classifierpath); err != nil {
+		return err
+	}
+
 	return nil
 }
